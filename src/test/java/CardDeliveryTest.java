deb0d0869de0ecba8fd98ecfb0cbdf62e7484264
YuliaPtitca <107066957+YuliaPtitca@users.noreply.github.com>
2022-10-09 11:21:25 +0300

    Update CardDeliveryTest.java

--- src/test/java/CardDeliveryTest.java
+++ src/test/java/CardDeliveryTest.java
@@ -25,14 +25,14 @@ public class CardDeliveryTest {
 
     @Test
     public void shouldFullAllFormCorrect() {
-
+        String date = generateDate(3);
         $("[data-test-id=city] input").setValue("Тула");
         $("[data-test-id=date] input").setValue(date);
         $("[data-test-id=name] input").setValue("Евдокиомов Павел");
         $("[data-test-id=phone] input").setValue("+79101255555");
         $("[data-test-id=agreement]").click();
         $(".button").click();
-        $(withText("Успешно!")).shouldBe(Condition.visible, Duration.ofSeconds(15));
+        $(withText("Успешно!")).shouldBe(visible, Duration.ofSeconds(15));
         $(".notification__content").shouldHave(exactText("Встреча успешно забронирована на " + date), Duration.ofSeconds(15))
                 .shouldBe(visible);
 
